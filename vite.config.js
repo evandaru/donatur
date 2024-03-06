b60600cb1aecd21bc555e035d9626cd56ce05210
evandaru <fznhsym@gmail.com>
2024-03-06 22:01:31 +0700

    anu3

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+
     proxy: {
+      '/foo': 'http://localhost:4567',
       '/api': {
         target: 'https://api.notion.com/v1/databases/f943b13338d643b3b91d9df822f1ed06/query',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-        onProxyReq: (proxyReq) => {
-          proxyReq.setHeader('Access-Control-Allow-Origin', '*');
-          proxyReq.setHeader('Access-Control-Allow-Methods', 'POST');
-          proxyReq.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        },
-      },
-      '/apicreate': {
-        target: 'https://api.notion.com/v1/pages',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/apicreate/, ''),
-        // Mengizinkan metode POST dari berbagai sumber
-
+        // onProxyReq: (proxyReq) => {
+        //   proxyReq.setHeader('Access-Control-Allow-Origin', '*');
+        //   proxyReq.setHeader('Access-Control-Allow-Methods', 'POST');
+        //   proxyReq.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        // },
       },
     },
   },
