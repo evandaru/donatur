515ef9c6a616f77e9e4442ac36fb048a0d4da08a
evandaru <fznhsym@gmail.com>
2024-03-06 23:56:57 +0700

    koawkoaw

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'https://donatur.vercel.app/',
     proxy: {
       '/api': {
         target: 'https://api.notion.com/v1/databases/f943b13338d643b3b91d9df822f1ed06/query',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         headers: {
-          Authorization: 'secret_dDkyTm301ESI6KmEYwLvZcoMrWWRqC9VRsxHftCwa6A',
+          Authorization: 'Bearer secret_dDkyTm301ESI6KmEYwLvZcoMrWWRqC9VRsxHftCwa6A',
           accept: 'application/json',
           'Notion-Version': '2022-06-28',
           'content-type': 'application/json'
