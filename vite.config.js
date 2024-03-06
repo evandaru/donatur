803a39b5d99f76e88cd6a2f5b7b9c7b1cfe087be
evandaru <fznhsym@gmail.com>
2024-03-06 23:02:30 +0700

    bismillah

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-
+    host: 'https://donatur.vercel.app/',
     proxy: {
       '/api': {
         target: 'https://api.notion.com/v1/databases/f943b13338d643b3b91d9df822f1ed06/query',
