fb75a14fbf61e4e77789649871764692a0e1cb14
evandaru <fznhsym@gmail.com>
2024-05-28 05:18:11 +0700

    bismillah perbaikan

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://donatur.vercel.app/',
+  base: '/',
   server: {
-    origin: 'https://donatur.vercel.app',
     proxy: {
-      '/foo': 'https://donatur.vercel.app',
       '/api': {
         target: 'https://api.notion.com/v1/databases/f943b13338d643b3b91d9df822f1ed06/query',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         headers: {
-          Authorization: 'secret_dDkyTm301ESI6KmEYwLvZcoMrWWRqC9VRsxHftCwa6A',
+          Authorization: 'Bearer secret_dDkyTm301ESI6KmEYwLvZcoMrWWRqC9VRsxHftCwa6A',
           accept: 'application/json',
           'Notion-Version': '2022-06-28',
           'content-type': 'application/json'
